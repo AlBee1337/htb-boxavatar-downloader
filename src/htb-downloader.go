085cd2b3f997945fdb02a1d29026324dfb98b443
albee <haxr.albee@gmail.com>
2022-12-05 16:29:08 +0100

    update

--- src/htb-downloader.go
+++ src/htb-downloader.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
+	"github.com/cavaliercoder/grab/v3"
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-
+	src := ""
 	c := colly.NewCollector()
-
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	isTrue := false
+	isTrueOneTime := true
+	c.OnHTML("img", func(e *colly.HTMLElement) {
+		src = e.Attr("src")
+		isTrue, _ = regexp.MatchString("https://www.hackthebox.com/storage/avatars/[[:ascii:]]+.png", src)
+		if isTrue && isTrueOneTime {
+			// fmt.Printf(src)
+			donwloadedAvatar, _ := grab.Get(".", src)
+			isTrueOneTime = false
+			fmt.Println(donwloadedAvatar)
+		}
+		// fmt.Println(src)
 	})
+	c.Visit("https://www.hackthebox.com/machines/steamcloud")
+	// fmt.Println(src)
 
-	c.Visit("http://webcode.me")
-
+	// match, _ := regexp.MatchString("peach", "p([a-z]+)ch")
+	// fmt.Println(match)
 }
